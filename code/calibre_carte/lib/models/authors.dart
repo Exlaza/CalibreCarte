e6ee9b0d68b714dfa2ce82a65613696290d01ef6
Ekansh Jain <ekansh.jain15@gmail.com>
2019-09-27 21:13:04 +0530

    feat: added model classes for authors, publishers, ratings and tags

--- code/calibre_carte/lib/models/authors.dart
+++ code/calibre_carte/lib/models/authors.dart
@@ -1,3 +1,24 @@
 class Authors {
+  int id;
+  String name;
+  String sort;
+  String link;
 
+  Authors({this.id, this.name, this.sort, this.link});
+
+  static final columns = [
+    "id",
+    "name",
+    "sort",
+    "link"
+  ];
+
+
+  //  Method to convert sqflite map to the model
+  Authors.fromMapObject(Map<String, dynamic> map) {
+    this.id = map['id'];
+    this.name = map['name'];
+    this.sort = map['sort'];
+    this.link = map['link'];
+  }
 }
